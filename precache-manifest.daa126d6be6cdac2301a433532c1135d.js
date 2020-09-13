ba686b8b061c564351d31d2ef12fcef70236c24a
RobDeadFreight <robertfranklindesign@gmail.com>
2020-09-13 11:21:31 +0100

    Updates

---
+++ precache-manifest.daa126d6be6cdac2301a433532c1135d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faafa407e38452ceafdd6372278fe9a8",
+    "url": "/robdeadfreight.github.io/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robdeadfreight.github.io/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9103fe1fb99e060b7ceb",
+    "url": "/robdeadfreight.github.io/robofriends/static/css/main.184942ef.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robdeadfreight.github.io/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robdeadfreight.github.io/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9103fe1fb99e060b7ceb",
+    "url": "/robdeadfreight.github.io/robofriends/static/js/main.1f7f4958.chunk.js"
+  },
+  {
+    "revision": "ff597125f92733fc3df8",
+    "url": "/robdeadfreight.github.io/robofriends/static/js/runtime-main.6115d916.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robdeadfreight.github.io/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
